e297518f70f27d8bae8403c307d3b23838377269
Vallar <tkalenko.yura15@gmail.com>
2018-05-29 16:58:32 +0300

    add animation view-1, view-2

--- www/templates/view-1/view-1.controller.js
+++ www/templates/view-1/view-1.controller.js
@@ -9,34 +9,33 @@
 
     function ViewOneController($state, $timeout) {
         var vm = this;
-        // vm.goView2 = goView2;
         vm.shangeScreen = shangeScreen;
 
+        var titleBlock = document.querySelector('.title-block');
+        var back = document.querySelector('.back-block');
         vm.item = {
             header: 'Gett Denta',
-            subheader: 'Emergency all over the world',
-            img: '',
-            description: '',
-            show: 0
+            subheader: 'Emergency all over the world ',
+            show: 0,
+            description: ''
         }
-        let titleBlock = document.querySelector('.title-block');
-        let back = document.querySelector('.back');
+
         function shangeScreen() {
             if(vm.item.show === 0){
-                titleBlock.classList.add('mov-up');
                 back.classList.add('mov-down');
+                $timeout(function () {
+                    titleBlock.classList.add('mov-up');
+                }, 1000)
                 $timeout(function () {
                     vm.item = {
                         header: 'Gett Denta',
                         subheader: 'Welcome Aboard',
                         show: 1,
-                        img: '',
-                        description: 'lorem as;dlkf ;lalskjf ;laskdjf;laksjdf;lkjsd;lfkjs;dlfk a;lkjsd;lfkja slkdfj a;lskdj f;laskjd f;lkaj sd;lfj a' +
-                        'a;sldk fj;alsk dj;aksj d;lfk jas;ldfj a;lsd jflaksjdf;lkas d; fkjasdl;k fjasl;dj f;lasjdfl;asj dl;fkja sdlkf alksd flkasj dfl;kja s' +
-                        ' a;lsdkfj ;askdj ;laksjd f;lksjdflkja s;dlkjfas;lkdj f;lkasjd;llksj ;dfkja sldkjf ;alskdjf;kasjd;lfkja ;lkj '
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur deserunt dolor doloremque dolorum iste iure labore libero natus nemo nesciunt quae quidem, repellendus sit soluta ullam voluptatibus! Consequatur, consequuntur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur deserunt dolor doloremque dolorum iste iure labore libero natus nemo nesciunt quae quidem, repellendus sit soluta ullam voluptatibus! Consequatur, consequuntur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur deserunt dolor doloremque dolorum iste iure labore libero natus nemo nesciunt quae quidem, repellendus sit soluta ullam voluptatibus! Consequatur, consequuntur.'
                     }
                     back.classList.remove('mov-down');
-                }, 2000);
+                    back.classList.add('back-block-2');
+                }, 2300);
             } else if(vm.item.show === 1){
                 $state.go('login');
             }
