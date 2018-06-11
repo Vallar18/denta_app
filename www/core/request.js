0398ee88586c5b46b8bd78069da6a91508a0172d
VadimUA <puet12vadim@gmail.com>
2018-06-11 16:00:14 +0300

    help example, fix request

--- www/core/request.js
+++ www/core/request.js
@@ -4,13 +4,11 @@
         .module('factory.request', ['ngStorage'])
         .factory('http', http);
 
-    http.$inject = ['$rootScope', '$http', '$q', '$sessionStorage', '$localStorage', 'toastr', '$state'];
+    http.$inject = ['$rootScope', '$http', '$q', '$sessionStorage', '$localStorage', 'toastr', '$state','$ionicLoading'];
 
-    function http($rootScope, $http, $q, $sessionStorage, $localStorage, toastr, $state) {
+    function http($rootScope, $http, $q, $sessionStorage, $localStorage, toastr, $state,$ionicLoading) {
         let request = function (method, url, data) {
-
             $rootScope.loading = true;
-
             let config = {
                 dataType: 'json',
                 method: method,
@@ -19,11 +17,9 @@
                     'Content-Type': 'application/json; charset=UTF-8'
                 }
             };
-
             if (method === 'POST' && data.responseType) {
                 config.responseType = data.responseType;
             }
-
             if (method === 'GET') {
                 config.params = data;
                 // if ($sessionStorage.locale) {
@@ -39,7 +35,6 @@
                 //     config.data.locale = $sessionStorage.locale;
                 // }
             }
-
             if ($sessionStorage.auth_key) {
                 url = url + '?auth_key=' + $sessionStorage.auth_key;
                 if ($localStorage.locale) {
@@ -49,65 +44,68 @@
                     // }
                 }
             }
-
             if ($sessionStorage.token) {
                 config.url = url;
                 // config.headers.Authorization = `Token ${$sessionStorage.token}`;
                 config.headers.Authorization = 'Token ' + $sessionStorage.token;
-
             } else {
                 config.url = url;
             }
-
-            return $http(config).then(
-                function (response) {
-                    $rootScope.loading = false;
-                    let defer = $q.defer();
-                    // console.clear();
-                    console.info('response', url, response);
-                    if (response.data.error) {
-                        toastr.error(response.data.error);
-                        defer.reject(response.data.error);
-                    }
-                    else {
-                        defer.resolve(response.data);
-                    }
-                    return defer.promise;
-                },
-                function (response) {
-                    console.info('error', url, response);
-                    let defer = $q.defer();
-
-                    if (response.status === 200) {
-                        toastr.error('Server Error: ' + response.data);
-                        defer.reject(response.data);
-                    }
-                    else if (response.status === -1) {
-                        toastr.error('Server unavailable');
-                        defer.reject(response.data);
-                    }
-                    else if (response.status === 500) {
-                        toastr.error(response.data.message);
-                        // toastr.error('Server Error: ' + response.status + ' ' + response.data.message);
-                        defer.reject(response.data);
-                    }
-                    else if (response.status === 403) {
-                        toastr.error('Access denied.');
-                        defer.reject(response.data);
-                    }
-                    else {
-                        if (response.status === 401) {
-                            $state.go('app.pages_auth_login');
-                        }
-                        // toastr.error('Server Error: ' + response.status + ' ' + response.data.message);
-                        toastr.error(response.data.message);
-                        defer.reject(response.data);
-                    }
-                    defer.reject(response.data);
-                    return defer.promise;
-                }
-            );
+            $ionicLoading.show({
+                template: 'Loading...',
+            });
+            return $http(config).then( requestSuccess(response), requestError(response));
         };
+
+        function requestSuccess(response) {
+            $rootScope.loading = false;
+            let defer = $q.defer();
+            // console.clear();
+            console.info('response', url, response);
+            if (response.data.error) {
+                toastr.error(response.data.error);
+                defer.reject(response.data.error);
+            }
+            else {
+                defer.resolve(response.data);
+            }
+            $ionicLoading.hide();
+            return defer.promise;
+        }
+        function requestError(response) {
+            console.info('error', url, response);
+            let defer = $q.defer();
+
+            if (response.status === 200) {
+                toastr.error('Server Error: ' + response.data);
+                defer.reject(response.data);
+            }
+            else if (response.status === -1) {
+                toastr.error('Server unavailable');
+                defer.reject(response.data);
+            }
+            else if (response.status === 500) {
+                toastr.error(response.data.message);
+                // toastr.error('Server Error: ' + response.status + ' ' + response.data.message);
+                defer.reject(response.data);
+            }
+            else if (response.status === 403) {
+                toastr.error('Access denied.');
+                defer.reject(response.data);
+            }
+            else {
+                if (response.status === 401) {
+                    $state.go('app.pages_auth_login');
+                }
+                // toastr.error('Server Error: ' + response.status + ' ' + response.data.message);
+                toastr.error(response.data.message);
+                defer.reject(response.data);
+            }
+            $ionicLoading.hide();
+            defer.reject(response.data);
+            return defer.promise;
+        }
+
         let requestFile = function (url, data) {
             $rootScope.loading = true;
             console.log(data);
--- www/templates/help/help.ctrl.js
+++ www/templates/help/help.ctrl.js
@@ -5,26 +5,46 @@
             .module('app')
             .controller('HelpCtrl', HelpCtrl);
 
-        HelpCtrl.$inject = ['$scope', '$ionicModal', '$cordovaContacts', '$ionicPlatform'];
+        HelpCtrl.$inject = ['$scope', '$ionicModal', '$cordovaContacts', '$ionicPlatform','$cordovaCamera'];
 
-        function HelpCtrl($scope, $ionicModal, $cordovaContacts, $ionicPlatform) {
+        function HelpCtrl($scope, $ionicModal, $cordovaContacts, $ionicPlatform, $cordovaCamera) {
             var vm = this;
 
             vm.pickContactUsingNativeUI = function () {
                 $ionicPlatform.ready(function () {
                     $cordovaContacts.pickContact().then(function (contactPicked) {
                         vm.phoneNumbers = contactPicked.phoneNumbers;
-                    }, function(error){
+                    }, function (error) {
 
                     })
                 });
             };
 
-            vm.getCameraPic  = function(){
+            vm.getCameraPic = function () {
                 $ionicPlatform.ready(function () {
+                    var options = {
+                        quality: 100,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: Camera.PictureSourceType.CAMERA,
+                        allowEdit: false,
+                        encodingType: Camera.EncodingType.JPEG,
+                        // targetWidth: 100,
+                        // targetHeight: 100,
+                        popoverOptions: CameraPopoverOptions,
+                        saveToPhotoAlbum: false,
+                        correctOrientation: true
+                    };
 
-                });
+                    $cordovaCamera.getPicture(options).then(function (imageData) {
+                        var image = document.getElementById('myImage');
+                        image.src = "data:image/jpeg;base64," + imageData;
+                    }, function (err) {
+                        // error
+                    });
+                }, false);
             }
+
+
         }
     }
 )();
