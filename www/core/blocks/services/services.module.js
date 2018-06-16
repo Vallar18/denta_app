2cbd994b4d3550ee468ab7e54067f1052cb78cbb
VadimUA <puet12vadim@gmail.com>
2018-06-17 00:47:42 +0300

    add change color of input

--- www/core/blocks/services/services.module.js
+++ www/core/blocks/services/services.module.js
@@ -12,6 +12,9 @@
         'service.tabsSvc',
         'service.specSvc',
         'service.currencySvc',
-        'service.phoneSvc'
+        'service.phoneSvc',
+        'service.profileSvc',
+        'service.reviewSvc',
+        'service.helpSvc'
     ]);
 })();
---
+++ www/services/profile.svc.js
@@ -0,0 +1,12 @@
+;(function () {
+    'use strict';
+
+    angular.module('service.profileSvc', []).factory('profileSvc', profileSvc);
+
+    profileSvc.$inject = [];
+
+    function profileSvc() {
+        var model = {};
+        return model;
+    }
+})();
