fd9f78927a86a31893d5291b3da9f2fbd375dd7e
VadimUA <puet12vadim@gmail.com>
2018-06-20 18:19:21 +0300

    fix merge

--- www/templates/dentist-profile/dentist-profile.ctrl.js
+++ www/templates/dentist-profile/dentist-profile.ctrl.js
@@ -5,9 +5,13 @@
         .module('app')
         .controller('DentistProfileCtrl', DentistProfileCtrl);
 
-    DentistProfileCtrl.$inject = [];
+    DentistProfileCtrl.$inject = ['userSvc'];
 
-    function DentistProfileCtrl() {
+    function DentistProfileCtrl(userSvc) {
         var vm = this;
+        vm.country = 'Israel';
+        vm.profile = userSvc.getUser();
+        vm.test = [ {name: 'tdddd est'}, {name: 'tefg dfg st'},{name: 'tdf vfdv est'}, {name: 'gdfgdfgdfgdfg '},{name: 'test'}, {name: 'test'},{name: 'test'}, {name: 'test'}];
+
     }
 })();
