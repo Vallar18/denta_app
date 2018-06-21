b958b9f7e6f25b2b84592155aaf3891eb8f9812f
VadimUA <puet12vadim@gmail.com>
2018-06-21 12:17:16 +0300

    fix merge

--- www/templates/dentist-profile/dentist-profile.ctrl.js
+++ www/templates/dentist-profile/dentist-profile.ctrl.js
@@ -13,5 +13,28 @@
         vm.profile = userSvc.getUser();
         vm.test = [ {name: 'tdddd est'}, {name: 'tefg dfg st'},{name: 'tdf vfdv est'}, {name: 'gdfgdfgdfgdfg '},{name: 'test'}, {name: 'test'},{name: 'test'}, {name: 'test'}];
 
+        vm.reviewArr = [
+            {
+                id:1,
+                date: '12 03 1093',
+                rating: 4,
+                name: 'Vasylyi',
+                text: 'Test test tskjnfkvn dsvknlkdfjvn sdkn vdslfk vndkfjvndknsdfkjvndklfvnskdfjvnksdjnvdfvkjnkjdfvn sd'
+            },
+            {
+                id:2,
+                date: '12 03 1093',
+                rating: 4,
+                name: 'Vasylyi',
+                text: 'Test test tskjnfkvn dsvknlkdfjvn sdkn vdslfk vndkfjvndknsdfkjvndklfvnskdfjvnksdjnvdfvkjnkjdfvn sd'
+            },
+            {
+                id:3,
+                date: '12 03 1093',
+                rating: 4,
+                name: 'Vasylyi',
+                text: 'Test test tskjnfkvn dsvknlkdfjvn sdkn vdslfk vndkfjvndknsdfkjvndklfvnskdfjvnksdjnvdfvkjnkjdfvn sd'
+            },
+        ];
     }
 })();
